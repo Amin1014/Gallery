5f342f0e9e9ec9dd5992dfe381fa981715bd1f6e
Amin1014 <aminabdi570@gmail.com>
2021-09-04 07:32:00 +0300

    adding category model

--- picha/models.py
+++ picha/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+#category model
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name 
+
+    def save_category(self):
+        self.save()
+    
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def update_category(cls, search_term , new_cat):
+        try:
+            to_update = Category.objects.get(name = search_term)
+            to_update.name = new_cat
+            to_update.save()
+            return to_update
+        except Category.DoesNotExist:
+            print('Category you specified does not exist')
