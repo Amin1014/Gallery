28973751eddabad39002e3a7df78fc5ddac2f294
Amin1014 <aminabdi570@gmail.com>
2021-09-04 07:51:19 +0300

    adding image category model

--- picha/models.py
+++ picha/models.py
@@ -31,3 +31,44 @@ class Images(models.Model):
     description = models.TextField()
     category=models.ForeignKey(Category, on_delete=models.CASCADE)
     location = models.ForeignKey('Locations', on_delete=models.CASCADE, default=1)
+
+    # add image categories model
+    def __str__(self):
+        return self.title
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(self, new_url):
+        try:
+            self.image_link = new_url
+            self.save()
+            return self
+        except self.DoesNotExist:
+            print('Image you specified does not exist')    
+    
+
+    @classmethod
+    def get_all(cls):
+        pics = Images.objects.all()
+        return pics
+    
+    
+    @classmethod
+    def get_image_by_id(cls, id):
+        retrieved = Images.objects.get(id = id)
+        return retrieved
+
+    @classmethod
+    def search_image(cls, cat):
+        retrieved = cls.objects.filter(category__name__icontains=cat) 
+        return retrieved #list of instances
+
+    @classmethod
+    def filter_by_location(cls ,location):
+        retrieved = Images.objects.filter(location__city__icontains=location)
+        return retrieved 
