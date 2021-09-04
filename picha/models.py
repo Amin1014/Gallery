80f4de658f580da32684f3b11ee136e925b7afc5
Amin1014 <aminabdi570@gmail.com>
2021-09-04 07:52:38 +0300

    adding location to model

--- picha/models.py
+++ picha/models.py
@@ -71,4 +71,29 @@ class Images(models.Model):
     @classmethod
     def filter_by_location(cls ,location):
         retrieved = Images.objects.filter(location__city__icontains=location)
-        return retrieved 
+        return retrieved
+
+#locations model
+class Locations(models.Model):
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.city
+
+    def save_location(self):
+        self.save()    
+
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def update_location(cls, search_term , new_locale):
+        try:
+            to_update = Locations.objects.get(country = search_term)
+            to_update.city = new_locale
+            to_update.save()
+            return to_update
+        except Locations.DoesNotExist:
+            print('Location you specified does not exist') 
+     
