7827f6e606f87982b21f56a5bb2c12d4ea46526b
Amin1014 <aminabdi570@gmail.com>
2021-09-04 07:29:24 +0300

    adding definition of views

--- picha/views.py
+++ picha/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
 from .models import Images, Locations
+
 # Create your views here.
+def homepage(request):
+    return render(request, 'index.html')
+
+def gallery(request):
+    locations =Locations.objects.all()
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        pictures = Images.search_image(search_term)
+        return render(request, 'gallery.html', {'pictures': pictures,'locations':locations})
+    else:
+        pictures = Images.get_all()
+        return render(request, 'gallery.html', {'pictures': pictures,'locations':locations})
+
+def search(request,city):
+    locations =Locations.objects.all()
+
+    pictures = Images.filter_by_location(city)
+    return render(request, 'gallery.html', {'pictures': pictures,'locations':locations})
 
