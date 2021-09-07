a8350f233df22dc7145c74b2ee58a54dfc3e9907
Amin1014 <aminabdi570@gmail.com>
2021-09-07 13:14:50 +0300

    adding tests

--- picha/tests.py
+++ picha/tests.py
@@ -16,10 +16,10 @@ class ImagesTest(TestCase):
         self.new_location = Locations(city='Nairobi', country='Kenya')
         self.new_location.save_location()
         
-        self.new_picture = Images(image_link='images/picture.jpeg', title='Image title', description='sth random', category=self.new_category, location=self.new_location)
-        self.new_picture.save_image()
-        self.another_picture = Images(image_link='images/photo.jpg', title='Another title', description='sth else more random', category=self.new_category, location=self.new_location)
-        self.another_picture.save_image()
+        # self.new_picture = Images(image_link='images/picture.jpeg', title='Image title', description='sth random', category=self.new_category, location=self.new_location)
+        # self.new_picture.save_image()
+        # self.another_picture = Images(image_link='images/photo.jpg', title='Another title', description='sth else more random', category=self.new_category, location=self.new_location)
+        # self.another_picture.save_image()
 
     def tearDown(self):
         '''
