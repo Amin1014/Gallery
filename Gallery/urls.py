e12ea5bf47647b91151fd69aec452505a89d154c
Amin1014 <aminabdi570@gmail.com>
2021-09-07 09:46:30 +0300

    adding urls in project

--- Gallery/urls.py
+++ Gallery/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+# from django.contrib import admin
+import picha
 from django.contrib import admin
+from django.urls import path,include
+# from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+    url('admin/', admin.site.urls),
+    path('',include('picha.urls',namespace='picha'))
+] 
