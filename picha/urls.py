6b3391dfa53e0ff8eab128390cdbeee524ca1691
Amin1014 <aminabdi570@gmail.com>
2021-09-07 09:51:32 +0300

    adding urls.py

--- picha/urls.py
+++ picha/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-app_name='picha'
+app_name='Gallery'
 urlpatterns=[
     path('', views.homepage, name='landing'), 
     path('gallery/', views.gallery, name='gallery'),
@@ -10,3 +10,6 @@ urlpatterns=[
     path('location/<str:city>', views.search, name='search')
 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
